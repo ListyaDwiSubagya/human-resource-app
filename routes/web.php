4e72c770d935583ca44932ec0588e648b7f82c79
ListyaDwiSubagya <listyodwi062@gmail.com>
2025-12-29 22:40:46 +0700

    fitur employee

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+// handle employee
+Route::resource('/employee', EmployeeController::class);
 
 // handle tasks
 Route::resource('/tasks', TaskController::class);
